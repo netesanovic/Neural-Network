cf78f2799ad44313cdeeb3fabff78e4214817226
Nemanja Tesanovic <ntesanovic@student.tgm.ac.at>
2018-01-05 16:57:13 +0100

    created a record for the neural network

---
+++ NN/Program.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open System
+
+type NeuralNetwork = 
+    {
+        top: int list
+        network: float array array
+        input: float list
+        weights: float array array array
+        biases: float array array
+    }
+
+
+[<EntryPoint>]
+let main argv = 
+
+    let neuralnetwork = 
+        {
+            top     = [2; 3; 1]
+            network = [||]
+            input   = [7.0; 15.0]
+            weights = [||]
+            biases  = [||]
+        }
+
+    Console.ReadLine() |> ignore
+
+    0 // return an integer exit code
