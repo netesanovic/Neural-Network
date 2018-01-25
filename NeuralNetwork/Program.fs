869532243237148a1cf1cb39cf8a27099eadb1c0
ntesanovic-tgm <ntesanovic@student.tgm.ac.at>
2018-01-25 09:39:04 +0100

    small changes

--- NeuralNetwork/Program.fs
+++ NeuralNetwork/Program.fs
@@ -11,6 +11,7 @@ type NeuralNetwork =
         input: float list
         weights: float array array array
         biases: float array array
+        target: float list
     }
    
 let normalizeInputs nn = 
@@ -33,7 +34,11 @@ let createBiases nn =
     let updatedNetwork = { nn with biases = newBiases }
     updatedNetwork
 
-let sigmoid (inpu: float) = 1.0 / (1.0 + 2.7 ** (-inpu))
+let setTarget nn targets= 
+    let updatedNetwork = {nn with target = targets}
+    updatedNetwork
+
+let sigmoid (inpu: float) = 1.0 / (1.0 + exp (-inpu))
     
 let propagate_forward nn = 
 
@@ -51,13 +56,39 @@ let propagate_forward nn =
 
     for x in 0..(nn.network.Length-2) do
         for y in 0..(nn.network.[x+1].Length-1) do
-            updateThese.[x+1].[y] <- sigmoid(nn.network .[x+1].[y])
+            updateThese.[x+1].[y] <- sigmoid(nn.network.[x+1].[y])
 
     let updatedNetwork = { nn with network = updateThese }
     updatedNetwork
 
-//let errors nn = 
-    
+let singleerr tar out = 
+    //0.5 * ((tar - out)**2.0)
+    out * (1.0 - out) * (tar - out)
+
+let errors nn = 
+    let l = nn.network.Length
+    let errlist = List.init nn.target.Length (fun x -> singleerr nn.target.[x] nn.network.[l-1].[x])
+    errlist
+
+let errtotal nn = 
+    let errlist = errors nn
+    List.sum errlist
+   
+//let printerr nn = 
+//    let err = errtotal nn
+//    printfn "%A" err
+
+//backpropagation fehlt
+// let propagate_backwards nn = 
+//     let updatedBiases  = nn.biases
+//     let updatedWeights = nn.weights
+
+//     let errlist = errors nn
+//     let error = errtotal nn
+
+//     for x in (nn.top.Length - 2) .. -1 .. 0 do
+
+//     0
 
 [<EntryPoint>]
 let main argv = 
@@ -69,6 +100,7 @@ let main argv =
             input   = [1.0; 1.0]
             weights = [||]
             biases  = [||]
+            target = [0.0]
         }
 
     let updated nn = 
@@ -78,8 +110,8 @@ let main argv =
         |> createWeights
         |> createBiases
         |> propagate_forward
+        //|> propagate_backwards
         
-
     printfn "%A" (updated neuralnetwork)
 
     Console.ReadLine() |> ignore
